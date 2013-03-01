c08cc488b9bf2c80efede8e80bf1830a307f0d6a
yattom <tsutomu.yasui@gmail.com>
2013-03-01 16:25:06 +0900

    first web.py spike

---
+++ python/mkakeibo/web_main.py
@@ -0,0 +1,15 @@
+import web
+
+urls = ('/', 'index')
+
+class index:
+  def GET(self):
+    print "GET!!!"
+    return "<html><head><title>mkakeibo</title></head><body><h1>Hi</h1></body></html>"
+
+if __name__=='__main__':
+    print "run"
+    app = web.application(urls, globals())
+    app.run()
+
+
