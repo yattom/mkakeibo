b873183ca0d8e6583bc989488be1e5558b475419
yattom <tsutomu.yasui@gmail.com>
2013-03-03 01:59:12 +0900

    add fitnesse paver tasks

--- pavement.py
+++ pavement.py
@@ -1,5 +1,6 @@
 from paver.easy import *
 from paver.path import path
+import os
 
 @task
 @cmdopts([
@@ -13,3 +14,15 @@ def test(options):
 
     files = path('python/mkakeibo').walk('*.py')
     sh('nosetests %s %s'%(' '.join(opts), ' '.join(files)))
+
+
+@task
+def start_fitnesse(options):
+    os.chdir('test/fitnesse')
+    sh('java -jar fitnesse.jar -e 0 -p 20942')
+
+@task
+def fitnesse(options):
+    os.chdir('test/fitnesse')
+    sh('java -jar fitnesse.jar -p 20942 -c "MkakeiboTop.AcceptanceTests?test&format=text"')
+
