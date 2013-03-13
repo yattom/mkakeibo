f933c86ca37e8dd8f43d42851ce5e79b67625a3d
yattom <tsutomu.yasui@gmail.com>
2013-03-13 11:03:42 +0900

    in the process of automate build & deploy
    paver for building and releasable archive (said binary)
    fabric for creating environment to deploy

--- fabfile.py
+++ fabfile.py
@@ -1,20 +1,37 @@
 from fabric.api import *
+from fabric.contrib import files
 
+@task
 def deploy(build_id):
     env.filename = 'mkakeibo-%s.tar.gz'%(build_id)
     put(env.filename, env.filename)
 
 
+@task
 def create_environment():
     put('env/.profile', '.profile')
     run('mkdir -p work')
     with cd('work'):
-        install_python()
+        run('sudo apt-get install libssl-dev')
+        _install_python()
+        _install_python_packages()
 
 
-def install_python():
+@task
+def clear_environment():
+    run('rmdir -rf work bin lib shared include')
+
+
+def _install_python():
+    if files.exists('Python-2.7.3'):
+        return
     run('wget http://www.python.org/ftp/python/2.7.3/Python-2.7.3.tgz')
     run('tar zxvf Python-2.7.3.tgz')
     with cd('Python-2.7.3'):
         run('./configure --prefix $HOME')
         run('make install')
+
+def _install_python_packages():
+    run('wget http://python-distribute.org/distribute_setup.py')
+    run('python distribute_setup.py')
+    run('easy_install pip')
