9db1446bc78f865e793c49f1b29b7ed633d3f4a6
yattom <tsutomu.yasui@gmail.com>
2013-04-02 15:55:36 +0900

    display entry form (work in progress)

--- python/mkakeibo/web_main.py
+++ python/mkakeibo/web_main.py
@@ -1,11 +1,35 @@
+# encoding: utf-8
+
 import web
+import datetime
 
 urls = ('/', 'Index')
+render = web.template.render('templates/')
+
+CATEGORIES = [
+    (u'食', u'食'),
+    (u'', u'その他'),
+]
+
+entry_form = web.form.Form(
+    web.form.Dropdown(
+        name='month',
+        args=range(1,13),
+        value=datetime.date.today().month),
+    web.form.Dropdown(
+        name='day',
+        args=range(1,31),
+        value=datetime.date.today().day),
+    web.form.Dropdown(
+        name='category',
+        args=CATEGORIES
+    )
+)
 
 class Index:
   def GET(self):
     print "GET!!!"
-    return "<html><head><title>mkakeibo</title></head><body><h1>Hi</h1></body></html>"
+    return render.index(entry_form)
 
 if __name__=='__main__':
     print "run"
