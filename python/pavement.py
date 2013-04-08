94a194948468e6b61cf31ece6815641e3d289f76
yattom <tsutomu.yasui@gmail.com>
2013-04-08 11:32:03 +0900

    add 'paver test -v' option

--- python/pavement.py
+++ python/pavement.py
@@ -35,13 +35,17 @@ def stop():
 
 @task
 @cmdopts([
-    ('format=', 'f', 'output format [silent, verbose, xunit]')
+    ('format=', 'f', 'output format [silent, verbose, xunit]'),
+    ('verbose', 'v', 'verbose and stdout (nosetests -v -s)')
 ])
 def test(options):
     format = options.test.format if 'format' in options.test else 'silent'
     opts = ['--with-doctest']
     if format == 'verbose': opts.append('--verbose')
     if format == 'xunit': opts.append('--with-xunit')
+
+    if 'verbose' in options.test:
+        opts.append('-v -s')
 
     files = path('mkakeibo').walk('*.py')
     sh('nosetests %s %s'%(' '.join(opts), ' '.join(files)))
